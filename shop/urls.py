cbf8f951b83a8f84e92cc28c3e56ca6eb5f04772
Anand <anandrajpatel134@gmail.com>
2022-10-15 18:55:31 +0530

    till 23

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="Shop Home")
+    path("", views.index, name="Shop Home"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="Tracking Status"),
+    path("Search/", views.search, name="Search"),
+    path("productview/", views.productView, name="Product"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,33 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("About")
+    # return render(request, 'shop/index.html')
+
+
+def contact(request):
+    return HttpResponse("contact")
+    # return render(request, 'shop/index.html')
+
+
+def tracker(request):
+    return HttpResponse("tracker")
+    # return render(request, 'shop/index.html')
+
+
+def search(request):
+    return HttpResponse("search")
+    # return render(request, 'shop/index.html')
+
+
+def productView(request):
+    return HttpResponse("Product View")
+    # return render(request, 'shop/index.html')
+
+
+def checkout(request):
+    return HttpResponse("checkout")
+    # return render(request, 'shop/index.html')
